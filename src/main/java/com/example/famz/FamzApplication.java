c623a37305f5f2499e2bda0adc4f0cbb364c8376
venkateshdev22 <pvenkateshnaidu@gmail.com>
2024-10-24 13:19:05 +0530

    Initial App

---
+++ src/main/java/com/example/famz/FamzApplication.java
@@ -0,0 +1,13 @@
+package com.example.famz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/famz/FamzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.famz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
